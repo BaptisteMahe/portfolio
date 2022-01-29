d8b06cf48426b9c25942cb6fcd2debeaf39b2e6e
BaptisteMahe <mahe.baptiste.19@gmail.com>
2022-01-29 20:50:18 +0100

    Add cool animations and stuff

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,14 @@ const sketch = (p5: P5) => {
   let hideFlockButton: HTMLButtonElement;
   let hideFlock = false;
 
-  let addOnClick = true;
+  let hideContentButton: HTMLButtonElement;
+  let hideContent = false;
+
+  let addBoidOnClick = true;
+
+  let contentContainer: HTMLDivElement;
+  let fadeInElements: HTMLElement[] = [];
+  let contactLinks: NodeListOf<HTMLAnchorElement>;
 
   p5.setup = () => {
     const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
@@ -21,22 +28,54 @@ const sketch = (p5: P5) => {
     hideFlockButton = document.querySelector("#hide-flock-button");
     hideFlockButton.onclick = () => {
       hideFlock = !hideFlock;
-      hideFlockButton.textContent = `${ hideFlock ? 'Show' : 'Hide' } flock`
+      hideFlockButton.textContent = `${ hideFlock ? 'Show' : 'Hide' } flock`;
     }
+
+    contentContainer = document.querySelector(".content-container");
+
+    hideContentButton = document.querySelector("#hide-content-button");
+    hideContentButton.onclick = () => {
+      hideContent = !hideContent;
+      hideContentButton.textContent = `${ hideContent ? 'Show' : 'Hide' } text`;
+      hideContent ? contentContainer.classList.add('hidden') : contentContainer.classList.remove('hidden');
+    }
+
+    document.querySelectorAll(".stacks-content > a > img")
+        .forEach(elem => fadeInElements.push(elem as HTMLElement));
+    document.querySelectorAll(".projects-list > a")
+        .forEach(elem => fadeInElements.push(elem as HTMLElement));
+    fadeInElements.push(document.querySelector("#clean-code-img"));
+    contactLinks = document.querySelectorAll(".social-networks > a");
+
+    contentContainer.onscroll = () => {
+      setOpacityOnScroll(fadeInElements, contentContainer.scrollTop);
+      setOpacityOnScroll(contactLinks, contentContainer.scrollTop, 1);
+
+      if (contentContainer.scrollTop + window.innerHeight >= contentContainer.scrollHeight)
+        contactLinks.forEach(contactLink => contactLink.classList.add("contact-link-animation"));
+      else contactLinks.forEach(contactLink => contactLink.classList.remove("contact-link-animation"));
+    }
+    setOpacityOnScroll(fadeInElements, contentContainer.scrollTop);
 
     flock = new Flock(p5, FLOCK_START_SIZE, FLOCK_MAX_SIZE);
   };
 
   p5.draw = () => {
     p5.background(0);
-
-    if (!hideFlock) flock.update(addOnClick);
+    if (!hideFlock) flock.update(addBoidOnClick);
   };
 
   p5.windowResized = () => {
     p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
     flock.resizeTank(p5.windowWidth, p5.windowHeight);
+    setOpacityOnScroll(fadeInElements, contentContainer.scrollTop);
   };
 };
 
 new P5(sketch);
+
+function setOpacityOnScroll(elements: HTMLElement[] | NodeListOf<HTMLElement>, currentScroll: number, heightCoef = 2) {
+  elements.forEach(element => {
+    element.style.opacity = String((currentScroll + window.innerHeight - element.offsetTop) / (heightCoef * element.scrollHeight))
+  });
+}
